577f8fbf7f84ed09ae6e1b3d1a80d2f43e5592f6
Stan Mayer <sthumor@gmail.com>
2024-11-03 19:13:33 +0200

    HW37_reverse

--- reverse-length-client/src/main/java/telran/net/Main.java
+++ reverse-length-client/src/main/java/telran/net/Main.java
@@ -1,7 +1,55 @@
 package telran.net;
 
+import telran.view.*;
+import org.json.*;
+
 public class Main {
+    static EchoClient echoClient;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Item[] items = {
+            Item.of("Start session", Main::startSession),
+            Item.of("Exit", Main::exit, true)
+      };
+      Menu menu = new Menu("Echo Application", items);
+      menu.perform(new StandardInputOutput());
+    }
+
+    public static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname: ");
+        Integer port = io.readNumberRange("Enter port: ", "Wrong port", 1, 65536).intValue();
+        if(echoClient != null) {
+            echoClient.close();
+        }
+        echoClient = new EchoClient(host, port);
+        Item[] items = {
+            Item.of("Get your string reversed", Main::getReverse),
+            Item.of("Get length of your string", Main::getLength),
+            Item.ofExit()
+      };
+        Menu menu = new Menu("Session is running", items);
+        menu.perform(io);
+
+    }
+
+    static void getReverse(InputOutput io) {
+        String response = outString(io, 1);
+        io.writeLine(response);
+    }
+
+    static void getLength(InputOutput io) {
+        String response = outString(io, 2);
+        io.writeLine(response);
+    }
+
+    private static String outString(InputOutput io, Integer code) {
+        String json = new JSONObject().put(code.toString(), io.readString("Enter any string: ")).toString();
+        return echoClient.sendAndReceive(json);
+    }
+
+    static void exit(InputOutput io) {
+        if(echoClient != null) {
+            echoClient.close();
+        }
     }
 }
--- reverse-length-server/src/main/java/telran/net/Main.java
+++ reverse-length-server/src/main/java/telran/net/Main.java
@@ -1,7 +1,38 @@
 package telran.net;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.json.*;  
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+       private static final int PORT = 5000;
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            runSession(socket);
+        }
+    }
+
+    private static void runSession(Socket socket) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintStream writer = new PrintStream(socket.getOutputStream())) {
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                JSONObject json = new JSONObject(line);
+                if(json.has("1")) {
+                    writer.println(new StringBuffer(json.getString("1")).reverse().toString());
+                }
+                else if(json.has("2")) {
+                    writer.println(json.getString("2").length());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Client closed connection abnormally");
+        }
     }
 }
